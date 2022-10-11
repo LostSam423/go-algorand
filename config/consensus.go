eddaa2da983c7e9c4ecb44a34c600d7767ada7e7
Sambit Behera <54500512+LostSam423@users.noreply.github.com>
2022-10-11 05:31:24 +0530

    Update consensus.go

--- config/consensus.go
+++ config/consensus.go
@@ -984,7 +984,7 @@ func initConsensusProtocols() {
 	v24.MaxGlobalSchemaEntries = 64
 
 	// Maximum cost of ApprovalProgram/ClearStateProgram
-	v24.MaxAppProgramCost = 200000
+	v24.MaxAppProgramCost = 100000000
 
 	// Maximum number of apps a single account can create
 	v24.MaxAppsCreated = 10
